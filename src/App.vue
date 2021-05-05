5ced21ed8db89e32c88c3fa22c4de356466e5210
mmilano <mmilano+git@gmail.com>
2021-05-04 19:49:25 -0700

    some demo

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,88 @@
 <template>
-  <div id="app">
+  <main id="app">
     <Hello msg="Spark Chart Donut"/>
-  </div>
+
+  <h2>one</h2>
+    <div class="donut1">
+      <SparkDonut v-bind="$options.donutExample1"></SparkDonut>
+    </div>
+
+    <h2>two</h2>
+    <div class="donut2">
+      <SparkDonut v-bind="$options.donutExample2"></SparkDonut>
+    </div>
+
+  </main>
 </template>
 
 <script>
 import Hello from './components/Hello.vue';
+import SparkDonut from './components/SparkDonut.vue';
 
 export default {
   name: "App",
 
   components: {
-    Hello
+    Hello,
+    SparkDonut
   },
 
+  data() {
+    return {};
+  },
+
+  donutExample1: {
+    value: 35,
+    colorForeground: "#33F",
+  },
+
+  donutExample2: {
+    value: 55,
+    colorForeground: "#33F",
+    stroke: 5,
+    counterClockwise: false
+  },
+
+  computed: {
+  },
+
+  mounted() {
+  },
 }
 </script>
 
 <style lang="scss">
+// .container {
+//   width: 220px;
+//   border: 1px dotted #999;
+//   svg {
+//     width: 100%;
+//   }
+// }
+
+.donut1 {
+  width: 200px;
+}
+
+.donut2 {
+  width: 200px;
+}
+
+
+
+.o-spark {
+  border: 1px dotted #999;
+  width: 260px;
+}
+
+.o-donut-background {
+  stroke: #DDD;
+}
+
+.o-donut-arc {
+    transform: rotate(-180deg);
+    transform-origin: center;
+    stroke: #20894f;
+}
 
 </style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,12 +2,27 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
 
+
+    <h2>Donut Chart properties</h2>
+
+    <ul>
+      <li>r = 50</li>
+      <li>calculated r' = r - (stroke-width/2)</li>
+      <li>Percentage fill = 25</li>
+      <li>C = 2 * π * r = 314.15</li>
+      <li>C' = 2 * π * r' = 251.32</li>
+      <li>Stroke length = (C/ 100) * (Percentage fill) = 78.54</li>
+      <li>Stroke length' = (C'/ 100) * (Percentage fill) = 62.83</li>
+    </ul>
+
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'Hello',
+
   props: {
     msg: String
   }
@@ -15,18 +30,18 @@ export default {
 </script>
 
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+// h3 {
+  // margin: 40px 0 0;
+// }
+// ul {
+  // list-style-type: none;
+  // padding: 0;
+// }
+// li {
+  // display: inline-block;
+  // margin: 0 10px;
+// }
+// a {
+  // color: #42b983;
+// }
 </style>
