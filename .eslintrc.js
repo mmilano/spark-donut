d0b725759c5723cba5bc0dcc13b88999b202d556
mmilano <mmilano+git@gmail.com>
2022-05-30 10:28:16 -0700

    linting changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,45 @@
 module.exports = {
     root: true,
-
     env: {
         node: true,
         browser: true,
     },
-
+    extends: ["plugin:vue/essential", "@vue/standard"],
     parserOptions: {
         ecmaVersion: 2020,
+        parser: "@babel/eslint-parser",
     },
-
     rules: {
-        'no-console': 'off',
-        'no-debugger': 'off',
-        'vue/html-end-tags': 'error',
-        'vue/html-quotes': 'error',
-        'vue/no-v-html': 'error',
-        'vue/no-unused-components': 'warn',
-        'vue/no-unused-vars': 'warn',
-        'vue/attribute-hyphenation': 'warn',
-        'vue/html-closing-bracket-newline': 'warn',
-        'vue/html-closing-bracket-spacing': 'warn',
-        'vue/mustache-interpolation-spacing': 'error',
-        'vue/require-name-property': 'error',
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        quotes: ["warn", "double", { allowTemplateLiterals: true }],
+        "comma-dangle": [
+            "warn",
+            {
+                arrays: "never",
+                objects: "always",
+                imports: "never",
+                exports: "never",
+                functions: "never",
+            },
+        ],
+        semi: ["error", "always"],
+        "no-multiple-empty-lines": ["error", { max: 3 }],
+        "space-before-function-paren": [
+            "error",
+            {
+                anonymous: "never",
+                named: "never",
+                asyncArrow: "never",
+            },
+        ],
     },
-
     overrides: [
         {
-            files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+            files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
             env: {
                 jest: true,
             },
         },
     ],
-
-    extends: [
-        "plugin:vue/essential",
-        "eslint:recommended"
-    ],
-
-    'extends': [
-      'plugin:vue/recommended',
-      'eslint:recommended'
-    ]
 };
